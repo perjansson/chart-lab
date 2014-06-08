92bd1939033e8e3ba3a368ed377f2c1274f47183
Per Jansson <per.r.jansson@gmail.com>
2014-06-08 19:36:25 +0200

    Added javadoc to chart provider.

--- src/main/java/com/peejay/chart/jensoftapi/ChartProvider.java
+++ src/main/java/com/peejay/chart/jensoftapi/ChartProvider.java
@@ -3,6 +3,10 @@ package com.peejay.chart.jensoftapi;
 import com.peejay.chart.jensoftapi.horizontalbar.HorizontalBarChart;
 import org.springframework.stereotype.Service;
 
+/**
+ * The sole purpose of this class is to extract the creating of the JenSoftApi specific chart class, so it can be mocked
+ * out during test of the chart specific factory.
+ */
 @Service
 public class ChartProvider {
 
