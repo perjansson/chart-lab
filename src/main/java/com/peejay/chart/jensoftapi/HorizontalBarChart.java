544ea36d8268236c8ed0ffe61be6e6693cb58f51
Per Jansson <per.r.jansson@gmail.com>
2014-06-10 20:22:33 +0200

    Switched to stack bar.

--- src/main/java/com/peejay/chart/jensoftapi/HorizontalBarChart.java
+++ src/main/java/com/peejay/chart/jensoftapi/HorizontalBarChart.java
@@ -1,20 +1,21 @@
 package com.peejay.chart.jensoftapi;
 
-import java.awt.*;
-import java.awt.image.BufferedImage;
-
-import com.jensoft.core.plugin.symbol.BarSymbol;
+import com.jensoft.core.palette.*;
+import com.jensoft.core.plugin.stripe.StripePlugin;
+import com.jensoft.core.plugin.stripe.painter.StripePalette;
+import com.jensoft.core.plugin.symbol.*;
 import com.jensoft.core.plugin.symbol.BarSymbol.MorpheStyle;
-import com.jensoft.core.plugin.symbol.BarSymbolLayer;
-import com.jensoft.core.plugin.symbol.SymbolComponent;
-import com.jensoft.core.plugin.symbol.SymbolPlugin;
 import com.jensoft.core.plugin.symbol.SymbolPlugin.SymbolNature;
 import com.jensoft.core.plugin.symbol.painter.draw.BarDefaultDraw;
 import com.jensoft.core.plugin.symbol.painter.effect.*;
 import com.jensoft.core.plugin.symbol.painter.fill.BarFill1;
+import com.jensoft.core.plugin.symbol.painter.fill.BarFill2;
 import com.jensoft.core.view.View2D;
 import com.jensoft.core.window.Window2D;
 import com.peejay.chart.Chart;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class HorizontalBarChart extends View2D implements Chart {
 
@@ -37,17 +38,23 @@ public class HorizontalBarChart extends View2D implements Chart {
     }
 
     public void addBar(String name, Double base, Double value) {
-        BarSymbol barSymbol = new BarSymbol();
-        barSymbol.setThemeColor(Color.BLACK);
-        barSymbol.setThickness(40);
-        barSymbol.setBase(base + 0);
-        barSymbol.setAscentValue(value);
-        barSymbol.setName(name);
-        barSymbol.setSymbol(name);
+        StackedBarSymbol barSymbol = new StackedBarSymbol();
+        barSymbol.setThemeColor(Color.WHITE);
+        barSymbol.setBase(0);
+        barSymbol.setAscentValue(base + value);
+        barSymbol.setThickness(25);
+
+        Stack offsetStack = new Stack("offset", NanoChromatique.LIGHT_GRAY, base);
+        barSymbol.addStack(offsetStack);
+
+        Stack valueStack = new Stack(name, NanoChromatique.DARK_GRAY, value);
+        barSymbol.addStack(valueStack);
+
         barSymbol.setMorpheStyle(MorpheStyle.Rectangle);
-        barSymbol.setBarDraw(new BarDefaultDraw());
         barSymbol.setBarFill(new BarFill1());
-        barSymbol.setBarEffect(new BarEffect3());
+        //barSymbol.setBarDraw(new BarDefaultDraw());
+        //barSymbol.setBarEffect(new BarEffect1());
+        //barSymbol.setRound(5);
         barLayer.addSymbol(SymbolComponent.createStrut(BarSymbol.class, 10));
         barLayer.addSymbol(barSymbol);
     }
@@ -56,5 +63,4 @@ public class HorizontalBarChart extends View2D implements Chart {
     public BufferedImage asBufferedImage(int width, int height) {
         return this.getImageView(width, height);
     }
-
 }
